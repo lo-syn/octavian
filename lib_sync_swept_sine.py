03296f95ad6f5b39f6097162d12af9c06d0b3ed4
lo-syn <laurence.porter@pm.me>
2024-02-03 22:05:50 +0100

    Add windowing for fadein/out

--- lib_sync_swept_sine.py
+++ lib_sync_swept_sine.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+from scipy import signal
 
 from lib_sig_process import wav_export
 
@@ -10,6 +11,23 @@ def swept_sine(f1, f2, fs, time):
 
     return x
 
+def sine_sweep_windowing(sine_sweep_array, fs):
+    fd1 = int(fs / 10)
+    window= signal.windows.cosine(fd1-1)
+    fade = np.arange(0, fd1-1, dtype=int)
+    #fade in = (1−cos((0:fd1−1)/fd1*pi))/2;
+
+    fade_in = (1 - np.cos(fade/fd1*math.pi))/2
+    fade_out = np.flip(fade_in)
+    idx = np.arange(1,fd1)
+    sine_sweep_array[idx]=sine_sweep_array[idx] * fade_in
+    sine_sweep_array[-fd1+1::]=sine_sweep_array[-fd1+1::] * fade_out
+
+    return sine_sweep_array
+
 x = swept_sine(20,20000,48000,1)
 
-#wav_export(x, 48000, "sine")
+x = sine_sweep_windowing(x, 48000)
+
+
+wav_export(x, 48000, "sine")
