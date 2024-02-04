c7db35f8808341767049f930230e19fdd57aecfd
lo-syn <laurence.porter@pm.me>
2024-02-04 11:07:23 +0100

    Add deconvolve function and descriptions

--- lib_sync_swept_sine.py
+++ lib_sync_swept_sine.py
@@ -1,17 +1,24 @@
 import math
 import numpy as np
 from scipy import signal
+from scipy.fft import fft, ifft
 
-from lib_sig_process import wav_export
+from lib_sig_process import wav_export, amplify_dbfs
+from lib_sig_plot import *
+
+FS = 48000
 
 def swept_sine(f1, f2, fs, time):
     L = time / math.log(f2/f1)
     t = np.arange(0,(fs*time)-1)/ fs
     x = np.sin(2* math.pi *f1 *L*(np.exp(t/L)))
 
-    return x
+    return x, L
 
 def sine_sweep_windowing(sine_sweep_array, fs):
+    '''
+    This function applies a cosine window to fs/10 at the start and end of the signal
+    '''
     fd1 = int(fs / 10)
     window= signal.windows.cosine(fd1-1)
     fade = np.arange(0, fd1-1, dtype=int)
@@ -25,9 +32,54 @@ def sine_sweep_windowing(sine_sweep_array, fs):
 
     return sine_sweep_array
 
-x = swept_sine(20,20000,48000,1)
+def deconvolve(input_array, L, f1, fs):
 
-x = sine_sweep_windowing(x, 48000)
+    length = 2**math.ceil(math.log2(len(input_array)))
 
+    Y = np.fft.fft(input_array,length) / fs 
 
-wav_export(x, 48000, "sine")
+    f_ax = np.linspace(0, fs, length+1)
+    spectrum = np.zeros_like(f_ax, dtype=np.complex_)
+    #  X = 2*sqrt(f ax/L).*exp(−j*2*pi* f ax*L.*(1−log(f ax/f1)) + j*pi/4) .                      
+    spectrum[1:] = 2*np.sqrt(f_ax[1:]/L) * np.exp(-2j * np.pi*f_ax[1:]*L* (1 - np.log(f_ax[1:]/f1))+ 1j * np.pi/4)
+    #remainder, H = signal.deconvolve(Y, X)
+    ##2*sqrt(f_ax/L) .* exp(−j*2*pi*
+
+    H = Y*spectrum[1:]
+    #H = signal.deconvolve(input_array, X)
+    #H[1] = 0
+
+    h = np.fft.ifft(H) # Must be zymmetric?
+   #print(h)
+
+    
+    return h
+
+    
+
+x, L = swept_sine(20,20000,FS,1)
+
+sine_x = sine_sweep_windowing(x, FS)
+
+sine_x = amplify_dbfs(3, sine_x)
+
+h = deconvolve(sine_x, L, 20, FS)
+#y = np.array(abs(y))
+#x = np.array(abs(x))
+# 
+#freq_resp  = np.stack((y,x))
+#print(freq_resp[0])
+
+sine_sweep = np.vstack((sine_x,np.arange(0,len(sine_x))))
+
+#app = init_plot()
+#window = create_window()
+#add_freq_plot(freq_resp, window,1,2,3)
+#add_time_plot(sine_sweep, window)
+#app.exec_()
+import matplotlib.pyplot as plt
+plt.plot(h)
+plt.grid()
+plt.show()
+
+wav_export(h, 48000, "sine")
