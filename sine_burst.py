23669c7cbdb2dce4bf43413fce7ea7069022ff48
lo-syn <laurence.porter@pm.me>
2024-02-04 21:53:07 +0100

    Add space

--- sine_burst.py
+++ sine_burst.py
@@ -32,7 +32,7 @@ class SineBurst(object):
         from sine_burst import SineBurst
         sine_burst = SineBurst(1000, -3, 0.1, 48000, windowed=False)
         windowed_sine_burst = SineBurst(1000, -3, 0.1, 48000, windowed=True)
-        plt.plot(sine_burst.time_axis,sine_burst.signal)
+        plt.plot(sine_burst.time_axis, sine_burst.signal)
         plt.plot(windowed_sine_burst.time_axis, windowed_sine_burst.signal)
         plt.title('Sine Bursts')
         plt.ylabel('Amplitude')
--- test_waveform_burst.py
+++ test_waveform_burst.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from sine_burst import SineBurst
 sine_burst = SineBurst(1000, -3, 0.1, 48000, windowed=False)
 windowed_sine_burst = SineBurst(1000, -3, 0.1, 48000, windowed=True)
-plt.plot(sine_burst.time_axis,sine_burst.signal)
+plt.plot(sine_burst.time_axis, sine_burst.signal)
 plt.plot(windowed_sine_burst.time_axis, windowed_sine_burst.signal)
 plt.title('Sine Bursts')
 plt.ylabel('Amplitude')
