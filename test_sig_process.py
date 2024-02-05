3cddb919daa0ca5f240dcf171c37b53b094fdfa1
lo-syn <laurence.porter@pm.me>
2024-02-05 21:59:59 +0100

    Add fft function

--- test_sig_process.py
+++ test_sig_process.py
@@ -3,6 +3,8 @@ from sine_burst import SineBurst
 from sine_sweep import SineSweep
 from noise import Noise
 import numpy as np
+import math
+import scipy
 
 sine_burst = SineBurst(1000, -2, 1, 48000, windowed=False)
 sine_sweep = SineSweep(20,200, -3, 1, 48000, windowed=False)
@@ -47,14 +49,43 @@ def audio_parameter_calc(audio_object, print_out):
 
     return crest_factor, peak_db, rms_db
 
+def audio_fft_convert(audio_object, fs, save_to_file, file_name):
+    duration = len(audio_object.signal) / fs
+    fft_array = scipy.fft.fft(audio_object.signal)
+    fft_freq = scipy.fft.fftfreq(int(fs * duration), 1 / fs)
+
+    reference = max(abs(fft_array)) # 0dB becomes Maximum Amplitude
+    fft_db = []
+
+    for i in fft_array:
+        fft_db.append(20 * math.log10(abs(i) / reference))
+
+    fft_data_length = int(len(fft_freq) / 2)
+    fft_db = fft_db[:fft_data_length]
+    fft_freq = fft_freq[:fft_data_length]
+    fft_freq = np.array(fft_freq)
+    fft_db = np.array(fft_db)
+    audio_object.fft_freqs = fft_freq
+    audio_object.fft_db = fft_db
+
+    if save_to_file == True:
+        fft_data = np.stack(((fft_freq,fft_db)))
+        fft_data = np.transpose(fft_data)
+        np.savetxt(file_name+".csv", fft_data, header="Frequency (Hz), Level (dB)", delimiter=',')
+
+
 sine_burst = audio_slicer(sine_burst, 0, 0.55, sine_burst.samplerate)
 sine_sweep = audio_slicer(sine_sweep, 0, 0.55, sine_sweep.samplerate)
 white_noise = audio_slicer(white_noise, 0, 0.55, white_noise.samplerate)
 
-sine_burst = audio_amplify_dbfs(sine_burst, 3)
+sine_burst = audio_amplify_dbfs(sine_burst, 1)
 
 audio_parameter_calc(sine_sweep, True)
 print(sine_sweep.rms_db)
+
+audio_fft_convert(sine_burst, sine_burst.samplerate, True, "test_sine_fft")
+
+
 
 
 
