7345080153c2050a7ebbe4bb54748edf446eddd6
Laurence Porter <laurence.porter@holoplot.net>
2024-02-06 08:50:50 +0100

    Tidy up file structure

--- task_read_audio.py
+++
@@ -1,9 +0,0 @@
-from lib_audio_read import *
-from lib_sig_process import *
-from lib_sig_plot import *
-
-filename = r"C:\Users\laurence.porter\Documents\Test_Development\Octavian\10ms_burst.wav"
-
-audio_data = audio_file_read(filename)
-print(audio_data)
-
