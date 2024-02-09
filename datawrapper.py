0e6518da89b8e7d22ee5d099028c94c3810e5951
lo-syn <laurence.porter@pm.me>
2024-02-09 23:16:54 +0100

    Create datawrapper.py

---
+++ datawrapper.py
@@ -0,0 +1,49 @@
+from scipy.io import wavfile
+import numpy as np
+
+class DataWrapper(object):
+
+    '''
+    Accepts data and returns it as an Object
+    This is mainly just to allow all processing functions to work
+
+    Parameters
+    ----------
+    file_path : str
+        Path for file
+
+    Returns
+    -------
+    file : DataWrapper.signal
+        Returns a list of arrays with audio file signal
+
+    time_axis : AudioFile.time_axis
+        Returns a 1D array with the time axis
+
+    Examples
+    --------
+    >>> wrapped_data = DataWrapper(data)
+
+    .. plot::
+
+    '''
+
+    def __init__(self, 
+                data: list,
+                ):
+        DataWrapper._check_parameters(data)
+        self.run()
+    
+    def wrap_data(self):
+        '''
+        This function wraps data
+        '''
+        pass
+
+    def run(self):
+        self.wrap_data()
+        
+    @staticmethod
+    def _check_parameters(data):
+        """Checks the parameters for data, raises exceptions if necessary."""
+        pass # Should add a check that data is ok
